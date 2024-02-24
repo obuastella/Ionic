e8ccaf0e348ca5dd2b79a711d36c5e37aafed206
Obua Stella <stellaobua123@gmail.com>
2024-02-24 16:45:16 +0100

    Added trash bin icon

--- myApp/src/pages/List.tsx
+++ myApp/src/pages/List.tsx
@@ -1,17 +1,37 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonViewWillEnter,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import { Redirect, Route } from "react-router";
 import Settings from "./Settings";
+import { trashBinOutline } from "ionicons/icons";
 
 const List: React.FC = () => {
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [users, setUsers] = useState<any[]>([]);
+
+  // Works like useEffect
+  useIonViewWillEnter(async () => {
+    const users = await getUsers();
+    setUsers(users);
+    setLoading(false);
+  });
+  const getUsers = async () => {
+    const data = await fetch("https://randomuser.me/api?results=10");
+    const users = await data.json();
+    return users;
+  };
+
+  const clearList = () => {};
   return (
     <IonPage>
       <IonHeader>
@@ -19,7 +39,14 @@ const List: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton></IonMenuButton>
           </IonButtons>
+
           <IonTitle>List</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={trashBinOutline} color="danger" />
+              {/* <IonButtons>Clear</IonButtons> */}
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">ui goes here</IonContent>
